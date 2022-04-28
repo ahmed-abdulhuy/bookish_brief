8b575366877b11739d2fb931aa9150fdb47f22a6
ahmed-abdulhuy <ahmelhuy@gmail.com>
2022-04-28 23:12:54 +0200

    add blogs api call

--- src/components/Blogs/Blogs.tsx
+++ src/components/Blogs/Blogs.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Blogs.scss'
 
 
 export default function Blogs() {
+  useEffect( () => {
+    fetchItems();
+}, []);
+
+const [items, setItems] = useState([]);
+
+const fetchItems = async () => {
+    const data = await fetch('/api/blogs');
+    console.log(data)
+    const items = await data.json();
+    console.log(items)
+    setItems(items);
+
+};  
   return (
-    <div className='blogs'>Blogs</div>
-  )
+    <div className='blogs'>
+      {
+        items.map((blog:IBlog) => (
+          <div>
+            <h1>{blog.name}</h1>
+            <p>{blog.message}</p>
+          </div>
+        ))
+      }
+    </div>
+  );
+};
+
+
+interface IBlog {
+  "name": string
+  "message": string
 }
