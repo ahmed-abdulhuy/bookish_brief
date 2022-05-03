ea0ca201c61b1da93bdf362c7bc8aadd20f4434d
ahmed-abdulhuy <ahmelhuy@gmail.com>
2022-05-03 22:16:26 +0200

    do some styling

--- src/components/Blogs/Blogs.tsx
+++ src/components/Blogs/Blogs.tsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import './Blogs.scss'
+import { useFetchBlogsQuery } from '../../redux/blogsAPI';
 import { RootState } from '../../redux/store';
 import { useSelector } from 'react-redux';
-import { useFetchBlogsQuery } from '../../redux/blogsAPI';
-// import { get_all } from '../../redux/filterSlice'; 
+import React from 'react'
+import './Blogs.scss'
 
 export default function Blogs() {
   
   const filter = useSelector((state: RootState) => state.filter.filter)
-  
   const {data=[], isFetching} = useFetchBlogsQuery(filter);
 
   return (
     <div className='blogs'>
       {
-        data.map((blog:IBlog) => (
-          <div className="card" key={blog.id}> {blog.blog} </div>
-        ))
+        isFetching?<h1>loading..</h1> : 
+          data.map((blog:IBlog) => (
+            <div className="card" key={blog.id}> {blog.blog} </div>
+          ))
       }
     </div>
   );
