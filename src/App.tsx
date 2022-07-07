bc13c37cd057ac9f873dc0612155445c93c42a91
ahmed-abdulhuy <ahmelhuy@gmail.com>
2022-07-07 13:46:11 +0200

    chane layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import Header from './layout/Header/Header';
 import './App.scss';
-import Footer from './layout/Footer/Footer';
-import Centered from './layout/centered/Centered';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './Pages/HomePage/Home';
+import AddBook from './Pages/AddBook/AddBook';
+import Header from './layout/Header/Header';
 
 function App() {
   return (
     <Provider store={ store }>
-      <div className="App">
-        <Header />
-        <Centered />
-        <Footer />
-      </div>
+      <Router>
+      <Header />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/addBookPage' element={<AddBook />} />
+        </Routes>
+      </Router>
     </Provider>
   );
 }
