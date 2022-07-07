277b120c1bfbf0e5bf959695fa1411ce9aa780af
ahmed-abdulhuy <ahmelhuy@gmail.com>
2022-07-07 13:41:50 +0200

    add cors origin for fron-end

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routesHandler = require('./routes/handler');
-
+const cors = require("cors")
 const app = express();
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000'
+    })
+)
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routesHandler);
