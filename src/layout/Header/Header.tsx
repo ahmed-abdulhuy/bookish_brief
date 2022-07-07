4e3acc4a3850e34026080eaf042f31cd210ebec2
ahmed-abdulhuy <ahmelhuy@gmail.com>
2022-07-07 13:51:16 +0200

    rename files

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Header.scss'
+import { Link } from 'react-router-dom'
+
 
 export default function Header() {
   return (
     <div className='header'>
-      Header
+      <Link to="/addBookPage" > add new book</Link>
+      <Link to="/" >Home</Link>    
     </div>
   )
 }
